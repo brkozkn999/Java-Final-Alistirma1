26f572a153c6b237585bd130b57818bb534bcc94
brkozkn999 <166210859+brkozkn999@users.noreply.github.com>
2024-04-16 20:56:49 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
-package org.example;
+import org.apache.commons.lang3.StringUtils;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        printHello();
-        }
-    }
-    public static void printHello() {
-        System.out.println("Hello");
+        String orijinalString = "  aa          bb  cccc";
+        String duzeltilmisString = StringUtils.deleteWhitespace(orijinalString);
+        System.out.println(orijinalString + " | Orijinal Text (" + orijinalString.length() + ")");
+        System.out.println(duzeltilmisString + " | Düzeltilmiş Text (" + duzeltilmisString.length() + ")");
     }
 }
